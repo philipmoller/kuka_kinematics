fb9e71b2fbe685acdbbf845d94637ae603ddacf5
philipmoller <45422015+philipmoller@users.noreply.github.com>
2019-03-07 15:48:42 +0100

    Update kinetest.m

--- kinetest.m
+++ kinetest.m
@@ -9,7 +9,7 @@ L2=Link('alpha',90*pi/180,  'a', 25, 'd', 0,                           'modified
 L3=Link('alpha',  0,        'a',315, 'd', 0,    'offset', -90*pi/180,  'modified');
 L4=Link('alpha',90*pi/180,  'a',  35,'d', -365,                        'modified');
 L5=Link('alpha',-90*pi/180, 'a',  0, 'd', 0,                           'modified');
-L6=Link('alpha',90*pi/180,  'a',  0, 'd', 0, 'modified');
+L6=Link('alpha',90*pi/180,  'a',  0, 'd', 0,    'offset', -180*pi/180, 'modified');
 
 kuka = SerialLink([L1 L2 L3 L4 L5 L6], 'name', 'KUKA')
 q = [10 20 30 40 50 60]*pi/180;
